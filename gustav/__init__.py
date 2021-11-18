c41c5e9e7579b6154666967caaae3cc01a2eca25
j042 <jaewooklee042@gmail.com>
2021-11-18 11:14:18 +0100

    Rename version _version

--- gustav/__init__.py
+++ gustav/__init__.py
@@ -1,9 +1,9 @@
-from gustav import version
+from gustav import _version
 from gustav import mesh
 from gustav.mesh import Mesh
 
 
-__version__ = version.version
+__version__ = _version.version
 
 __all__ = [
     "__version__",
