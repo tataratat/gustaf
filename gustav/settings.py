a7764b9ac6636da498131ffd7378753e0e1f3b90
j042 <jaewooklee042@gmail.com>
2021-11-26 18:10:35 +0100

    Add DTYPES

--- gustav/settings.py
+++ gustav/settings.py
@@ -4,4 +4,8 @@ Global variables/constants that's used throughout `gustav`.
 """
 
 TOLERANCE = 1e-10
-VISUALIZATION_BACKEND = "vedo" # OPTIONS are <"vedo" | "trimesh">
+
+FLOAT_DTYPE="float64"
+INT_DTYPE="int32"
+
+VISUALIZATION_BACKEND = "vedo" # OPTIONS are <"vedo" | "trimesh" | "matplotlib">
