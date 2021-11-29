285fa3c0b1e30934be8e4708925ffdb4f17462af
j042 <jaewooklee042@gmail.com>
2021-11-29 08:54:02 +0100

    Edit __init__

--- gustav/__init__.py
+++ gustav/__init__.py
@@ -1,11 +1,21 @@
 from gustav import _version
-from gustav import mesh
 from gustav import settings
-from gustav.mesh import Mesh
+from gustav import vertices
+from gustav import edges
+from gustav import faces
+from gustav import volumes
+
+from gustav.vertices import Vertices
+from gustav.edges import Edges
+from gustav.faces import Faces
+from gustav.volumes import Volumes
 
 __version__ = _version.version
 
 __all__ = [
     "__version__",
-    "Mesh",
+    "Vertices",
+    "Edges",
+    "Faces",
+    "Volumes",
 ]
