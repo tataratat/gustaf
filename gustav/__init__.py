b79398d324fa5ed49093fb984abb52b4e6da1b47
j042 <jaewooklee042@gmail.com>
2022-01-05 10:45:33 +0100

    try to import spline

--- gustav/__init__.py
+++ gustav/__init__.py
@@ -8,13 +8,19 @@ from gustav import show
 from gustav import utils
 from gustav import create
 
-# import try/catch for spline
+try:
+    from gustav import spline
+    from gustav.spline.base import BSpline, NURBS
+except:
+    utils.log.debug("can't import spline related modules.")
+
 # import try/catch for triangle and gustav-tetgen
 
 from gustav.vertices import Vertices
 from gustav.edges import Edges
 from gustav.faces import Faces
 from gustav.volumes import Volumes
+
 
 __version__ = _version.version
 
