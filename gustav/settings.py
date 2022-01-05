6299749aa617535a13da95ab47e7e4423ce8b037
j042 <jaewooklee042@gmail.com>
2022-01-05 10:47:05 +0100

    Add vedo default options

--- gustav/settings.py
+++ gustav/settings.py
@@ -5,7 +5,18 @@ Global variables/constants that's used throughout `gustav`.
 
 TOLERANCE = 1e-10
 
-FLOAT_DTYPE="float64"
-INT_DTYPE="int32"
+FLOAT_DTYPE = "float64"
+INT_DTYPE = "int32"
 
 VISUALIZATION_BACKEND = "vedo" # OPTIONS are <"vedo" | "trimesh" | "matplotlib">
+
+VEDO_DEFAULT_OPTIONS = dict(
+    vertex=dict(
+    ),
+    edges=dict(
+    ),
+    faces=dict(
+    ),
+    volumes=dict(
+    ),
+)
