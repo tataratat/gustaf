d59f2ee56dbd9ab048b518ea54fdcc8a5a28b308
Jaewook Lee <jaewooklee042@gmail.com>
2022-01-28 21:01:48 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup
 with open("gustav/_version.py") as f:
     version = eval(f.read().strip().split("=")[-1])
 
+with open("README.md", "r") as f:
+    readme = f.read()
+
 setup(
     name="gustav",
     version=version,
     description="Fast geometry prototyper.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author="Jaewook Lee",
     author_email="jlee@ilsb.tuwien.ac.at",
     packages=[
