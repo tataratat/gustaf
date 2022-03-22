58370558ac3347eff38d4e2c31f2926ab8d773b6
Jaewook Lee <jaewooklee042@gmail.com>
2022-03-22 12:03:21 +0100

    add mixd load

---
+++ gustav/io/__init__.py
@@ -0,0 +1,12 @@
+"""
+gustav/gustav/io/__init__.py
+
+io.
+I - `load`.
+O - `export`.
+"""
+
+from gustav.io import ioutils
+from gustav.io import mfem
+from gustav.io import meshio
+from gustav.io import mixd
