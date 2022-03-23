e14b7c664963e7cab1cd95f164a1b6b673770888
Jaewook Lee <jaewooklee042@gmail.com>
2022-03-23 11:13:18 +0100

    extend show vis_dict support

--- gustav/show.py
+++ gustav/show.py
@@ -174,15 +174,62 @@ def _vedo_showable(obj, **kwargs):
     """
     import vedo
 
+    # parse from vis_dict
+    basic_options = dict(
+        c = obj.vis_dict.get("c", None),
+        r = obj.vis_dict.get("r", None),
+        lw = obj.vis_dict.get("lw", None),
+        alpha = obj.vis_dict.get("alpha", None),
+        #shrink = obj.vis_dict.get("shrink", None),
+        cmap = obj.vis_dict.get("cmap", None),
+        dataname = obj.vis_dict.get("dataname", None)
+    )
+    # loop once more to extract basics from kwargs
+    # done after vis_dict, so that this overpowers
+    keys = list(kwargs.keys()) # to pop dict during loop
+    for key in keys:
+        if key in basic_options.keys():
+            basic_options[key] = kwargs[key]
+            kwargs.pop(key)
+
     utils.log.debug("making vedo-showable obj")
+    local_options = dict()
+
     if obj.kind == "vertex":
-        return vedo.Points(obj.vertices, **obj.vis_dict, **kwargs)
+        for key in ["c", "r", "alpha"]:
+            value = basic_options[key]
+            if value is not None:
+                local_options.update({key : value})
+
+        vobj = vedo.Points(
+            obj.vertices,
+            **local_options,
+            **kwargs
+        )
 
     elif obj.kind == "edge":
-        return vedo.Lines(obj.vertices[obj.edges], **obj.vis_dict, **kwargs)
+        for key in ["c", "lw", "alpha"]:
+            value = basic_options[key]
+            if value is not None:
+                local_options.update({key : value})
+
+        vobj = vedo.Lines(
+            obj.vertices[obj.edges],
+            **local_options,
+            **kwargs
+        )
 
     elif obj.kind == "face":
-        return vedo.Mesh([obj.vertices, obj.faces], **obj.vis_dict, **kwargs)
+        for key in ["c", "alpha"]:
+            value = basic_options[key]
+            if value is not None:
+                local_options.update({key : value})
+
+        vobj = vedo.Mesh(
+            [obj.vertices, obj.faces],
+            **local_options,
+            **kwargs
+        )
 
     elif obj.kind == "volume":
         from vtk import VTK_TETRA as frau_tetra
@@ -196,20 +243,61 @@ def _vedo_showable(obj, **kwargs):
         else:
             return None # get_whatami should've rasied error.. 
 
-        vol = vedo.UGrid(
-            [
-                obj.vertices,
-                obj.volumes,
-                np.repeat([grid_type], len(obj.volumes)),
-            ]
-        )
+        ## shrink should be done at gustaf level, since it is available now!
+        # shrink? save here to allocate values accordingly
+        #shrink = basic_options["shrink"]
+        #if shrink is None:
+        #    shrink = kwargs.get("shrink", True) # maybe False?
 
-        if kwargs.get("shrink", True):
-            vol = vol.tomesh(shrink=.8)
+        #if shrink:
+        #    vobj = vobj.tomesh(shrink=.8)
+        #    if basic_options["c"] is None:
+        #        vobj.color("hotpink")
+        #    else:
+        #        vobj.color(basic_options["c"])
 
-        vol.color(kwargs.get("c", "hotpink"))
+        if basic_options["dataname"]:
+            from gustav.faces import Faces
 
-        return vol
+            # UGrid would be politically correct,
+            # but currently it can't show field
+            # so, extract only surface mesh
+            surf_ids = obj.get_surfaces() # gets faces too
+            sfaces = Faces(obj.vertices, obj.faces[surf_ids])
+            sfaces.remove_unreferenced_vertices(inplace=True)
+
+            vobj = sfaces.showable(backend="vedo") # recursive alert
+        
+        else:
+            vobj = vedo.UGrid(
+                [
+                    obj.vertices,
+                    obj.volumes,
+                    np.repeat([grid_type], len(obj.volumes)),
+                ]
+            )
+
+            if basic_options["c"] is None:
+                basic_options["c"] = "hotpink"
+
+            vobj.color(basic_options["c"])
+            vobj.alpha(basic_options["alpha"])
+
+    # this sets vedo v2021.0.6+ requirement
+    dname = basic_options["dataname"]
+    if dname is not None:
+        # transfer data
+        vobj.pointdata[dname] = obj.vertexdata[dname]
+
+        # default cmap is jet.
+        if basic_options["cmap"] is None:
+            basic_options["cmap"] = "jet"
+
+        # register cmap and data
+        vobj.cmap(basic_options["cmap"], dname)
+
+
+    return vobj
 
 
 def _trimesh_showable(obj):
