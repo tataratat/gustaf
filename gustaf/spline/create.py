46c447f1ecd9bfc7880147373847ac826b17a2e2
jzwar <zwar@ilsb.tuwien.ac.at>
2022-08-10 14:56:15 +0200

    Added test and adapted revolution routine
    3D Axis can now be set regardless of spline dimension.
    added unit tests for create

--- gustaf/spline/create.py
+++ gustaf/spline/create.py
@@ -5,43 +5,46 @@ Create operations for spline geometries
 
 
 import itertools
-import logging
 
 import numpy as np
 
+from splinepy._spline import _RequiredProperties
 
-def extrude(spline, axis=None):
+from gustaf import utils
+
+
+def extrude(spline, extrusion_vector=None):
     """
     Extrudes Splines
 
     Parameters
     ----------
     spline: GustafSpline
-    axis: np.ndarray
+    extrusion_vector: np.ndarray
     """
     from gustaf.spline.base import GustafSpline
-    
+
     # Check input type
     if not issubclass(type(spline), GustafSpline):
         raise NotImplementedError("Extrude only works for splines")
 
-    # Check axis
-    if axis is not None:
-        # make flat axis
-        axis = np.asarray(axis).ravel()
+    # Check extrusion_vector
+    if extrusion_vector is not None:
+        # make flat extrusion_vector
+        extrusion_vector = np.asarray(extrusion_vector).ravel()
     else:
-        raise ValueError("No extrusion Axis given")
+        raise ValueError("No extrusion extrusion_vector given")
 
-    # Check Axis dimension
+    # Check extrusion_vector dimension
     # formulate correct cps
-    if spline.dim == axis.shape[0]:
+    if spline.dim == extrusion_vector.shape[0]:
         cps = spline.control_points
-    elif spline.dim > axis.shape[0]:
-        expansion_dimension = axis.shape - spline.dim
+    elif spline.dim < extrusion_vector.shape[0]:
+        expansion_dimension = extrusion_vector.shape[0] - spline.dim
         # one smaller dim is allowed
         # warn that we assume new dim is all zero
         utils.log.warning(
-            "Given axis is f{expansion_dimension} dimension bigger than "
+            "Given extrusion vector is f{expansion_dimension} dimension bigger than "
             "spline's dim. Assuming 0.0 entries for new dimension.",
         )
         cps = np.hstack(
@@ -52,17 +55,17 @@ def extrude(spline, axis=None):
         )
     else:
         raise ValueError(
-            "Dimension Mismatch between extrusion axis and spline."
+            "Dimension Mismatch between extrusion extrusion vector and spline."
         )
 
     # Start Extrusion
     arguments = {}
 
     arguments["degrees"] = np.concatenate((spline.degrees, [1]))
-    arguments["control_points"] = np.vstack((cps, cps + axis))
-    if "knot_vectors" in spline._required_properties:
-        arguments["knot_vectors"] = spline.knot_vectors + [[0,0,1,1]]
-    if "weights" in spline._required_properties:
+    arguments["control_points"] = np.vstack((cps, cps + extrusion_vector))
+    if "knot_vectors" in _RequiredProperties.of(spline):
+        arguments["knot_vectors"] = spline.knot_vectors + [[0, 0, 1, 1]]
+    if "weights" in _RequiredProperties.of(spline):
         arguments["weights"] = np.concatenate((spline.weights, spline.weights))
 
     return type(spline)(**arguments)
@@ -94,54 +97,65 @@ def revolve(spline, axis=None, center=None, angle=None, n_knot_spans=None):
     if not issubclass(type(spline), GustafSpline):
         raise NotImplementedError("Revolutions only works for splines")
 
+    # Check axis
+    if axis is not None:
+        # Transform into numpy array
+        axis = np.asarray(axis).ravel()
+        # Check Axis dimension
+        if (spline.control_points.shape[1] > axis.shape[0]):
+            raise ValueError(
+                "Dimension Mismatch between extrusion axis and spline."
+            )
+        elif (spline.control_points.shape[1] < axis.shape[0]):
+            utils.log.warning(
+                "Control Point dimension is smaller than axis dimension,"
+                " filling with zeros"
+            )
+            expansion_dimension = axis.shape[0] - spline.dim
+            cps = np.hstack(
+                (
+                    spline.control_points,
+                    np.zeros((len(spline.control_points), expansion_dimension))
+                )
+            )
+        else:
+            cps = np.copy(spline.control_points)
+
+        # Make sure axis is normalized
+        axis = axis / np.linalg.norm(axis)
+    else:
+        cps = np.copy(spline.control_points)
+        if spline.control_points.shape[1] == 3:
+            raise ValueError("No rotation axis given")
+
+    # Set Problem dimension
+    problem_dimension = cps.shape[1]
+
+    # Init center
+    if center is not None:
+        center = np.asarray(center).ravel()
+        # Check Axis dimension
+        if not (problem_dimension == center.shape[0]):
+            raise ValueError(
+                "Dimension Mismatch between axis and center of rotation."
+            )
+        cps -= center
+
     # The parametric dimension is independent of the revolution but the
     # rotation-matrix is only implemented for 2D and 3D problems
-    if not (spline.dim == 2 or spline.dim == 3):
+    if not (cps.shape[1] == 2 or cps.shape[1] == 3):
         raise NotImplementedError(
             "Sorry,"
             "revolutions only implemented for 2D and 3D splines"
         )
 
-    # Check axis
-    if axis is not None:
-        if spline.dim == 2:
-            logging.warning("For 2D revolutions axis will be ignored")
-            axis=None
-        else:
-            axis = np.asarray(axis)
-            # Check Axis dimension
-            if not (spline.control_points.shape[1] == axis.shape[0]):
-                raise ValueError(
-                    "Dimension Mismatch between extrusion axis and spline."
-                )
-            # Make sure axis is normalized
-            axis = axis / np.linalg.norm(axis)
-    else:
-        if spline.control_points.shape[1] == 3:
-            raise ValueError("No rotation axis given")
-
-    # Init center
-    if center is not None:
-        center = np.asarray(axis)
-        # Check Axis dimension
-        if not (spline.control_points.shape[1] == center.shape[0]):
-            raise ValueError(
-                "Dimension Mismatch between extrusion center and spline."
-            )
-        spline.control_points -= center
-
     # Angle must be (0, pi) non including
     # Rotation is always performed in half steps
     PI = np.pi
-    if n_knot_spans is None:
-        n_knot_spans = int(np.ceil(np.abs((angle + 1e-13) / PI)))
-    else:
-        if n_knot_spans < int(np.ceil(np.abs((angle + 1e-13) / PI))):
-            logging.warning(
-                f"Number of segments was insufficient "
-                "- raised to {n_knot_spans}"
-            )
-            n_knot_spans = int(np.ceil(np.abs((angle + 1e-13) / PI)))
+    round_tolerance = 1e-13
+    minimum_n_knot_spans = int(np.ceil(np.abs((angle + round_tolerance) / PI)))
+    if (n_knot_spans) is None or (n_knot_spans < minimum_n_knot_spans):
+        n_knot_spans = minimum_n_knot_spans
 
     if "Bezier" in spline.whatami:
         if n_knot_spans > 1:
@@ -156,38 +170,37 @@ def revolve(spline, axis=None, center=None, angle=None, n_knot_spans=None):
     weight = np.sin(half_counter_angle)
     factor = 1 / weight
 
-
     # Assemble rotation matrix
-    if spline.dim == 2:
+    if problem_dimension == 2:
         rotation_matrix = np.array([
             [np.cos(rot_a), -np.sin(rot_a)],
             [np.sin(rot_a), np.cos(rot_a)]
         ]).T
     else:
-        # See rodrigues' formula
+        # See Rodrigues' formula
         rotation_matrix = np.array(
             [[0, -axis[2], axis[1]],
-            [axis[2], 0, -axis[0]],
-            [axis[1], axis[0], 0]]
+             [axis[2], 0, -axis[0]],
+             [axis[1], axis[0], 0]]
         )
         rotation_matrix = (np.eye(3) + np.sin(rot_a) * rotation_matrix + (
             (1 - np.cos(rot_a)) * np.matmul(rotation_matrix, rotation_matrix))
-            ).T
+        ).T
 
     # Start Extrusion
     arguments = {}
 
     arguments["degrees"] = np.concatenate((spline.degrees, [2]))
 
-    arguments["control_points"] = spline.control_points
-    end_points = spline.control_points
+    arguments["control_points"] = cps
+    end_points = cps
     for i_segment in range(n_knot_spans):
         # Rotate around axis
         mid_points = np.matmul(end_points, rotation_matrix)
         end_points = np.matmul(mid_points, rotation_matrix)
         # Move away from axis using dot-product tricks
-        if spline.dim == 3:
-            mp_scale = axis * np.dot(mid_points, axis).reshape(-1,1)
+        if problem_dimension == 3:
+            mp_scale = axis * np.dot(mid_points, axis).reshape(-1, 1)
             mid_points = (mid_points - mp_scale) * factor + mp_scale
         else:
             mid_points *= factor
@@ -197,29 +210,28 @@ def revolve(spline, axis=None, center=None, angle=None, n_knot_spans=None):
             end_points
         ))
 
-    if "knot_vectors" in spline._required_properties:
-        kv = [0,0,0]
+    if "knot_vectors" in _RequiredProperties.of(spline):
+        kv = [0, 0, 0]
         [kv.extend([i + 1, i + 1]) for i in range(n_knot_spans - 1)]
         arguments["knot_vectors"] = spline.knot_vectors + [
             kv + [n_knot_spans+1] * 3
         ]
-    if "weights" in spline._required_properties:
+    if "weights" in _RequiredProperties.of(spline):
         mid_weights = spline.weights * weight
         arguments["weights"] = spline.weights
         for i_segment in range(n_knot_spans):
-          arguments["weights"] = np.concatenate((
-              arguments["weights"],
-              mid_weights,
-              spline.weights
-          ))
+            arguments["weights"] = np.concatenate((
+                arguments["weights"],
+                mid_weights,
+                spline.weights
+            ))
     else:
-        logging.warning(
+        utils.log.warning(
             "True revolutions are only possible for rational spline types.\n"
             "Creating Approximation"
         )
 
     if center is not None:
-        spline.control_points += center
         arguments["control_points"] += center
 
     return type(spline)(**arguments)
---
+++ tests/test_create.py
@@ -0,0 +1,204 @@
+import gustaf as gus
+import numpy as np
+try:
+    from . import common as c
+except:
+    import common as c
+
+
+class ProximityTest(c.unittest.TestCase):
+
+    # Test Extrusion routines
+    def test_create_extrude(self):
+        """
+        Test extrusion for different input types and arguments
+        """
+        # make a couple of 2D splines
+        bspline = gus.BSpline(
+            control_points=c.CTPS_2D,
+            degrees=c.DEGREES_2D_NU,
+            knot_vectors=c.KV_2D
+        )
+        nurbs = gus.NURBS(
+            control_points=c.CTPS_2D,
+            degrees=c.DEGREES_2D_NU,
+            knot_vectors=c.KV_2D,
+            weights=c.WEIGHTS_2D
+        )
+        bezier = gus.Bezier(
+            control_points=c.CTPS_2D,
+            degrees=c.DEGREES_2D_U,
+        )
+        rationalbezier = gus.RationalBezier(
+            control_points=c.CTPS_2D,
+            degrees=c.DEGREES_2D_U,
+            weights=c.WEIGHTS_2D
+        )
+
+        # Expect Failure - not a spline
+        with self.assertRaises(NotImplementedError):
+            gus.spline.create.extrude([4])
+        # Expect Failure - no axis given
+        with self.assertRaises(ValueError):
+            bspline.create.extrude()
+        # Expect Failure - axis wrong format
+        with self.assertRaises(ValueError):
+            bspline.create.extrude(extrusion_vector=[1])
+
+        # Create a random axis
+        axis = np.random.rand(3)
+        x, y, z = np.random.rand(3).tolist()
+
+        # Test results
+        for spline_g in (bspline, nurbs, rationalbezier, bezier):
+            self.assertTrue(
+                np.allclose(
+                    bspline.create.extrude(extrusion_vector=axis).evaluate(
+                        [[x, y, z]]
+                    ),
+                    np.hstack((
+                        bspline.evaluate([[x, y]]),
+                        np.zeros((1, 1))
+                    )) + z * axis
+                )
+            )
+
+        # Create a random axis
+        axis = np.random.rand(3)
+        x, y, z = np.random.rand(3).tolist()
+
+        # Test results
+        for spline_g in (bspline, nurbs, rationalbezier, bezier):
+            self.assertTrue(
+                np.allclose(
+                    spline_g.create.extrude(extrusion_vector=axis).evaluate(
+                        [[x, y, z]]
+                    ),
+                    np.hstack((
+                        spline_g.evaluate([[x, y]]),
+                        np.zeros((1, 1))
+                    )) + z * axis
+                )
+            )
+
+    # Test Revolution Routine
+    def test_create_revolution(self):
+        """
+        Test revolution routines for different input types and arguments
+        """
+        # make a couple of 2D splines
+        bspline = gus.BSpline(
+            control_points=c.CTPS_2D,
+            degrees=c.DEGREES_2D_NU,
+            knot_vectors=c.KV_2D
+        )
+        nurbs = gus.NURBS(
+            control_points=c.CTPS_2D,
+            degrees=c.DEGREES_2D_NU,
+            knot_vectors=c.KV_2D,
+            weights=c.WEIGHTS_2D
+        )
+        bezier = gus.Bezier(
+            control_points=c.CTPS_2D,
+            degrees=c.DEGREES_2D_U,
+        )
+        rationalbezier = gus.RationalBezier(
+            control_points=c.CTPS_2D,
+            degrees=c.DEGREES_2D_U,
+            weights=c.WEIGHTS_2D
+        )
+
+        # Make some lines
+        bezier_line = gus.Bezier(
+            control_points=[
+                [1, 0],
+                [2, 1]
+            ],
+            degrees=[1]
+        )
+        nurbs_line = bezier_line.nurbs
+
+        # Make a cuboid
+        cuboid = gus.Bezier(
+            control_points=[
+                [0, 0, 0],
+                [1, 0, 0],
+                [0, 1, 0],
+                [1, 1, 0],
+                [0, 0, 1],
+                [1, 0, 1],
+                [0, 1, 1],
+                [1, 1, 1]
+            ],
+            degrees=[1, 1, 1]
+        )
+
+        # Expect Failure - not a spline
+        with self.assertRaises(NotImplementedError):
+            gus.spline.create.revolve([4])
+        # Expect Failure - No rotation axis
+        with self.assertRaises(ValueError):
+            cuboid.create.revolve()
+        # Expect Failure - axis wrong format
+        with self.assertRaises(ValueError):
+            bspline.create.revolve(axis=[1])
+
+        # Revolve always around z-axis
+        # init rotation matrix
+        r_angle = np.random.rand()
+        r_center = -np.random.rand(2)
+        r_center = np.array([1, 0])
+        cc, ss = np.cos(r_angle), np.sin(r_angle)
+        R = np.array([
+            [cc, -ss, 0],
+            [ss, cc, 0],
+            [0, 0, 1]
+        ])
+        R2 = np.array([[cc, -ss], [ss, cc]])
+
+        # Test 3D revolutions for bodies
+        for spline_g in (bspline, nurbs, rationalbezier, bezier):
+            self.assertTrue(
+                np.allclose(
+                    spline_g.create.revolve(
+                        axis=[0, 0, 1],
+                        center=[0, 0, 0],
+                        angle=r_angle
+                    ).control_points[-10:, :],
+                    np.matmul(
+                        np.hstack((
+                            spline_g.control_points,
+                            np.zeros((spline_g.control_points.shape[0], 1))
+                        )),
+                        R.T
+                    )
+                ),
+                f"{spline_g.whatami} failed revolution")
+
+        # Test 2D Revolutions of lines
+        for spline_g in (bezier_line, nurbs_line):
+            self.assertTrue(np.allclose(
+                spline_g.create.revolve(angle=r_angle).control_points[-2:, :],
+                np.matmul(
+                    spline_g.control_points,
+                    R2.T
+                )
+            ))
+
+        # Test 2D Revolutions of lines around center
+        for spline_g in (bezier_line, nurbs_line):
+            self.assertTrue(np.allclose(
+                spline_g.create.revolve(
+                    angle=r_angle,
+                    center=r_center
+                ).control_points[-2:, :],
+                np.matmul(
+                    spline_g.control_points - r_center,
+                    R2.T
+                ) + r_center
+            ),
+                f"{spline_g.whatami} failed revolution around center")
+
+
+if __name__ == "__main__":
+    c.unittest.main()
