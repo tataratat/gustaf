148aebb2769f45cccb80b43f5ddfdba642c3a1e9
Florian Zwicke <dev@zwicke.org>
2022-08-11 09:39:29 +0200

    Redesign group conversion functions.

--- gustaf/utils/groups.py
+++ gustaf/utils/groups.py
@@ -7,6 +7,50 @@ missing.
 """
 import numpy as np
 
+def element_to_vertex_group(element_connectivity, element_ids):
+    """
+    Take an element connectivity array and IDs referencing the faces and create
+    a corresponding list of unique vertices.
+
+    Elements could be edges, faces, or volumes.
+
+    Parameters
+    -----------
+    element_connectivity: (ne, nn) np.ndarray
+    element_ids: (n) np.ndarray
+
+    Returns
+    --------
+    vertex_ids: (n) np.ndarray
+    """
+    vertex_ids = np.unique(element_connectivity[element_ids])
+    return vertex_ids
+
+def vertex_to_element_group(element_connectivity, vertex_ids):
+    """
+    Take an element connectivity array and IDs referencing the vertices and
+    create a corresponding list of element IDs that contain all the vertices.
+
+    Note that this operation is not always uniquely defined. In contiguous
+    regions, individual elements could be left out even when all vertices are
+    used. This implementation uses a greedy approach that adds all possible
+    elements.
+
+    Parameters
+    -----------
+    element_connectivity: (ne, nn) np.ndarray
+    vertex_ids: (n) np.ndarray
+
+    Returns
+    --------
+    element_ids: (n) np.ndarray
+    """
+    # create array of booleans that are True for all the elements in the
+    # group
+    element_in_group = np.isin(element_connectivity, vertex_ids).all(axis=1)
+    element_ids = element_in_group.nonzero()[0]
+    return element_ids
+
 class VertexGroupCollection(dict):
     def __init__(self, mesh):
         """
@@ -118,8 +162,7 @@ class FaceGroupCollection(dict):
         """
         vertex_group = self.mesh.vertex_groups[group_name]
         all_faces = self.mesh.get_faces()
-        faces_in_group = np.isin(all_faces, vertex_group).all(axis=1)
-        self[group_name] = faces_in_group.nonzero()[0]
+        self[group_name] = vertex_to_element_group(all_faces, vertex_group)
 
     def import_all_vertex_groups(self):
         """
@@ -134,8 +177,9 @@ class FaceGroupCollection(dict):
         --------
         None
         """
-        for group_name in self.mesh.vertex_groups:
-            self.import_vertex_group(group_name)
+        all_faces = self.mesh.get_faces()
+        for group_name, vertex_ids in self.mesh.vertex_groups.items():
+            self[group_name] = vertex_to_element_group(all_faces, vertex_ids)
 
     def export_vertex_group(self, group_name):
         """
@@ -157,7 +201,8 @@ class FaceGroupCollection(dict):
         """
         all_faces = self.mesh.get_faces()
         face_group = self[group_name]
-        self.mesh.vertex_groups[group_name] = np.unique(all_faces[face_group])
+        self.mesh.vertex_groups[group_name] = element_to_vertex_group(
+                all_faces, face_group)
 
     def export_all_vertex_groups(self):
         """
@@ -172,6 +217,8 @@ class FaceGroupCollection(dict):
         --------
         None
         """
-        for group_name in self:
-            self.export_vertex_group(group_name)
+        all_faces = self.mesh.get_faces()
+        for group_name, face_ids in self.items():
+            self.mesh.vertex_groups[group_name] = element_to_vertex_group(
+                    all_faces, face_ids)
 
