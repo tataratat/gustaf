eaa5ba44ab142880a734fcb9bddd460566020e72
Florian Zwicke <dev@zwicke.org>
2022-08-12 11:07:49 +0200

    Simplexification example now shows all groups.

--- examples/diagonalize.py
+++ examples/diagonalize.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     tri = gus.create.faces.simplexify(quad)
 
-    show_options = [
+    option_plots = [
             ["no backslash, no alternate",
             gus.create.faces.simplexify(quad, alternate=False).shrink()],
             ["backslash, no alternate",
@@ -31,37 +31,41 @@ if __name__ == "__main__":
             gus.create.faces.simplexify(quad, backslash=True).shrink()]
             ]
 
-    try:
-        import vedo # if nothing's raised, following should be usable
-
-        gus.show.show_vedo(*show_options)
-
-        gus.show.show_vedo(*(
+    vertex_group_plots = (
                 [[vertex_group, quad.extract_vertex_group(vertex_group)] for
                     vertex_group in quad.vertex_groups] +
                 [[vertex_group, tri.extract_vertex_group(vertex_group)] for
-                    vertex_group in tri.vertex_groups] +
-                [[face_group, quad.extract_face_group(face_group).shrink()] for
-                    face_group in quad.face_groups] +
-                [[face_group, tri.extract_face_group(face_group).shrink()] for
-                    face_group in tri.face_groups]
-                ))
-    except:
-        for show_option in show_options:
-            print(f"Showing: {show_option[0]}")
-            show_option[1].show()
+                    vertex_group in tri.vertex_groups]
+                )
 
-        # show groups
-        for vertex_group in tri.vertex_groups:
-            print(f"Showing vertex group {vertex_group}.")
-            quad.extract_vertex_group(vertex_group).show()
-            tri.extract_vertex_group(vertex_group).show()
-        for face_group in tri.face_groups:
-            print(f"Showing face group {face_group}.")
-            quad.extract_face_group(face_group).shrink().show()
-            tri.extract_face_group(face_group).shrink().show()
+    edge_group_plots = (
+                [[edge_group, quad.extract_edge_group(edge_group).shrink()]
+                    for edge_group in quad.edge_groups] +
+                [[edge_group, tri.extract_edge_group(edge_group).shrink()]
+                    for edge_group in tri.edge_groups]
+                )
+
+    face_group_plots = (
+                [[face_group, quad.extract_face_group(face_group).shrink()]
+                    for face_group in quad.face_groups] +
+                [[face_group, tri.extract_face_group(face_group).shrink()]
+                    for face_group in tri.face_groups]
+                )
+
+    try:
+        import vedo
+        gus.show.show_vedo(*option_plots)
+        gus.show.show_vedo(*vertex_group_plots)
+        gus.show.show_vedo(*edge_group_plots)
+        gus.show.show_vedo(*face_group_plots)
+    except:
+        for item in (option_plots + vertex_group_plots +
+                edge_group_plots + face_group_plots):
+            print(f"Showing {item[0]}.")
+            item[1].show()
 
     # show edges
     e = tri.shrink(ratio=0.7).toedges(False)
     e.vis_dict["arrows"] = True
     e.show()
+
