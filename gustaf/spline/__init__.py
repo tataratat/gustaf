67ad410d92daea31c067f4221ce271b917c2f5dc
Jaewook Lee <jaewooklee042@gmail.com>
2022-09-06 11:26:27 +0200

    edit __init__

--- gustaf/spline/__init__.py
+++ gustaf/spline/__init__.py
@@ -9,5 +9,11 @@ However, since create.spline does not rely on `splinepy`, it is not here.
 from gustaf.spline import base
 from gustaf.spline import create
 from gustaf.spline import extract
-from gustaf.spline.base import show, from_mfem, load_splines
+from gustaf.spline.base import (Bezier,
+                                RationalBezier,
+                                BSpline,
+                                NURBS,
+                                show,
+                                from_mfem,
+                                load_splines)
 from gustaf.spline import ffd
