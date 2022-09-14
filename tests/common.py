44b43352286b8ffaf77ba868997a47f907bd5bfe
Jaewook Lee <jaewooklee042@gmail.com>
2022-09-14 14:07:59 +0200

    rm unused

--- tests/common.py
+++ tests/common.py
@@ -10,8 +10,6 @@ import numpy as np
 
 import gustaf as gus
 
-# gus.utils.log.configure(debug=True)
-
 # Mesh Stuff
 V = np.array(
     [
@@ -93,41 +91,6 @@ HV = np.array(
 )
 
 
-# class obj.
-VERT_MUSTER = gus.Vertices(
-    vertices=V
-)
-
-EDGE_MUSTER = gus.Edges(
-    vertices=V,
-    edges=E,
-)
-
-TRI_MUSTER = gus.Faces(
-    vertices=V,
-    faces=TF,
-)
-TET_MUSTER = gus.Volumes(
-    vertices=V,
-    volumes=TV,
-)
-QUAD_MUSTER = gus.Faces(
-    vertices=V,
-    faces=QF,
-)
-HEXA_MUSTER = gus.Volumes(
-    vertices=V,
-    volumes=HV,
-)
-
-
-# Spline Stuff
-B1P2D_MUSTER = gus.BSpline(
-    degrees=None,
-    knot_vectors=None,
-    control_points=None,
-)
-
 CTPS_2D = np.array(
     [
         [0, 0],
@@ -163,57 +126,4 @@ WEIGHTS_2D = np.array(
         [1.0]
     ],
     dtype=np.float64,
-)
-
-
-# alias
-logconfig = gus.utils.log.configure
-
-COMMON = namedtuple(
-    "COMMON",
-    [
-        "ut",
-        "np",
-        "gus",
-        "v",
-        "e",
-        "tf",
-        "qf",
-        "tv",
-        "hv",
-        "vert",
-        "edge",
-        "tri",
-        "tet",
-        "quad",
-        "hexa",
-        "ctps2d",
-        "kv2d",
-        "degrees2dnu",
-        "degrees2du",
-        "weights2s"
-    ]
-)
-
-C = COMMON(
-    ut=unittest,
-    np=np,
-    gus=gus,
-    v=V,
-    e=E,
-    tf=TF,
-    qf=QF,
-    tv=TV,
-    hv=HV,
-    vert=VERT_MUSTER,
-    edge=EDGE_MUSTER,
-    tri=TRI_MUSTER,
-    tet=TET_MUSTER,
-    quad=QUAD_MUSTER,
-    hexa=HEXA_MUSTER,
-    ctps2d=CTPS_2D,
-    kv2d=KV_2D,
-    degrees2dnu=DEGREES_2D_NU,
-    degrees2du=DEGREES_2D_U,
-    weights2s=WEIGHTS_2D
 )
