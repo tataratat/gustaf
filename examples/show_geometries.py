ab868305b4ca2b86c3be82442ccdee2c425a5557
Pohlmann, Roxana <roxana.pohlmann@tuwien.ac.at>
2022-10-04 11:02:51 +0200

    Add feature shapes

--- examples/show_geometries.py
+++ examples/show_geometries.py
@@ -3,10 +3,10 @@ import numpy as np
 
 if __name__ == '__main__':
 
+
     line = gus.spline.create.line(np.array([[0, 0, 0], [2, 5, 0], [4, 4, 2]]))
     rect = gus.spline.create.rectangle(5, 3)
     box = gus.spline.create.box(3, 2, 4)
-<<<<<<< HEAD
     pyramid = gus.spline.create.pyramid(1, 1, 2)
 
     # Show objects
@@ -39,25 +39,35 @@ if __name__ == '__main__':
 
     sphere = gus.spline.create.sphere(3)
     gus.show.show_vedo(['Sphere', sphere], resolution=50)
-=======
     pyramid = gus.spline.create.pyramid(2, 2, 6)
-
-    # gus.show.show_vedo(line, rect, box, pyramid)
+    
+    gus.show.show_vedo(
+        ['Line' , line], 
+        ['Rectangle', rect], 
+        ['Box', box],
+        ['Pyramid', pyramid], 
+        title='Rectangular objects')
 
     circ = gus.spline.create.circle(5)
     disk1 = gus.spline.create.disk(3, angle=100, n_knot_spans=2)
-    disk2 = gus.spline.create.disk(5, angle=360, n_knot_spans=10)
+    disk2 = gus.spline.create.disk(5, r0=1., angle=360, n_knot_spans=10)
 
-    # gus.show.show_vedo(circ, disk1, disk2)
+    gus.show.show_vedo(
+        ['Line Circle', circ], 
+        ['Disk, 2 knots spans', disk1], 
+        ['Disk, 10 knots spans', disk2])
 
     cone = gus.spline.create.cone(5, 10, angle=180)
-    cone.show()
-    sphere = gus.spline.create.sphere(3)
-    # sphere.show()
+    gus.show.show_vedo(['Cone', cone])
 
     torus = gus.spline.create.torus(5, 2, r0=0.5)
-    torus.show()
-
     torus2 = gus.spline.create.torus(5, 2, r0=0.5, angle=[100, 210])
+
     torus2.show()
->>>>>>> add example for shape creation routines
+    gus.show.show_vedo(
+        ['Torus', torus],
+        ['Incomplete torus', torus2]
+    )
+    
+    sphere = gus.spline.create.sphere(3)
+    gus.show.show_vedo(['Sphere', sphere])
