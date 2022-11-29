68cd47a26df5245c111d127bdf28b87c10b28425
LinusKnecht <linus.knecht@tuwien.ac.at>
2022-11-29 10:19:46 +0100

    updated version

--- examples/create_block_mesh.py
+++ examples/create_block_mesh.py
@@ -3,11 +3,11 @@ import gustaf as gus
 
 def main():
 
-    mesh_faces = gus.create.faces.quad_block_mesh(
+    mesh_faces = gus.create.faces.box(
             bounds=[[0, 0], [2, 2]], resolutions=[2, 3]
     )
 
-    mesh_volumes = gus.create.volumes.hexa_block_mesh(
+    mesh_volumes = gus.create.volumes.box(
             bounds=[[0., 0., 0.], [1., 1., 1.]], resolutions=[2, 3, 4]
     )
 
