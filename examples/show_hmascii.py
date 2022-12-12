e4405d8937aedef8dbb7f8a173171c50b3dadb5d
LinusKnecht <linus.knecht@tuwien.ac.at>
2022-12-12 15:09:14 +0100

    updated example

--- examples/show_hmascii.py
+++ examples/show_hmascii.py
@@ -3,11 +3,9 @@ import gustaf as gus
 
 def main():
     hm_tet = gus.io.hmascii.load("data/hm_tet.hmascii")
-    # hm_hex = gus.io.hmascii.load("data/hm_hex.hmascii")
-    hm_hex = gus.io.hmascii.load("data/test.py")
+    hm_hex = gus.io.hmascii.load("data/hm_hex.hmascii")
 
     gus.show.show_vedo(["hm_tet", hm_tet], ["hm_hex", hm_hex])
-
 
 if __name__ == "__main__":
     main()
