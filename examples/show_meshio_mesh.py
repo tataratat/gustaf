378780049de45ebef50fbc5f2b7c758f9c6d75fb
clemens.fricke <clemens.david.fricke@tuwien.ac.at>
2022-12-15 15:11:21 +0100

    format correction

--- examples/show_meshio_mesh.py
+++ examples/show_meshio_mesh.py
@@ -9,12 +9,13 @@ from gustaf import io
 
 if __name__ == "__main__":
 
-
     base_samples_path = pathlib.Path("samples/faces")
     if not base_samples_path.exists():
         raise RuntimeError(
-            "The geometries could not be found. Please initialize the samples "
-            "submodule, instructions can be found in the README.md.")
+                "The geometries could not be found. Please initialize the "
+                "samples submodule, instructions can be found in the "
+                "README.md."
+        )
     mesh_file_tri = pathlib.Path("samples/faces/tri/2DChannelTria.msh")
     mesh_file_quad = pathlib.Path("samples/faces/quad/2DChannelQuad.msh")
 
@@ -23,7 +24,7 @@ if __name__ == "__main__":
 
     loaded_mesh_tri.show()
 
-        # load the .msh file directly with the correct io module (meshio)
+    # load the .msh file directly with the correct io module (meshio)
     loaded_mesh_quad = io.meshio.load(mesh_file_quad)
 
     loaded_mesh_quad.show()
