e6aff776211be4407701a21978050ee9e6b3d993
LinusKnecht <linus.knecht@tuwien.ac.at>
2022-12-16 14:07:57 +0100

    restore

--- examples/show_hmascii.py
+++ examples/show_hmascii.py
@@ -4,22 +4,21 @@ import gustaf as gus
 def main():
     try:
         hm_tet = gus.io.hmascii.load(
-            "../../samples/volumes/tet/3DPipeCorner90Tet.hmascii"
+                "../../samples/volumes/tet/3DPipeCorner90Tet.hmascii"
         )
         hm_hex = gus.io.hmascii.load(
-            "../../samples/volumes/hex/3DPipeCorner90Hex.hmascii"
+                "../../samples/volumes/hex/3DPipeCorner90Hex.hmascii"
         )
 
-    except:
+    except BaseException:
         raise RuntimeError(
-            'Can`t find mesh in sample files. '
-            'Make sure that ``gustaf`` and ``samples`` directory are on '
-            'the same level!'
+                'Can`t find mesh in sample files. '
+                'Make sure that ``gustaf`` and ``samples`` directory are on '
+                'the same level!'
         )
 
     gus.show.show_vedo(
-        ["3DPipeCorner90Tet", hm_tet], 
-        ["3DPipeCorner90Hex", hm_hex]
+            ["3DPipeCorner90Tet", hm_tet], ["3DPipeCorner90Hex", hm_hex]
     )
 
 
