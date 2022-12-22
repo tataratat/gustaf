4722e705153f01fe03524a9d414f599cb20a548d
Lukas <lukas.thurner@hotmail.com>
2022-12-22 10:24:02 +0100

    Add pytest fo connec

--- tests/test_utils/test_connec.py
+++ tests/test_utils/test_connec.py
@@ -1,93 +1,72 @@
-import unittest
-import pytest
-import numpy as np
-
-from gustaf.utils.connec import tet_to_tri, hexa_to_quad
+from tests.fixtures.fixtures_connec import *
+from gustaf.utils.connec import tet_to_tri, hexa_to_quad, faces_to_edges, make_quad_faces, make_hexa_volumes
 
 
-class TestTetToTriFunction(unittest.TestCase):
-
-    def test_tet_to_tri_throwException(self):
-        volumes = np.array([0, 5, 4, 4, 6, 8, 8, 9])
-        tet_to_tri(volumes)
-
-    def test_tet_to_tri_expect_raiseValueError(self):
-        volumes = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [7, 8, 9]])
-        self.assertRaises(ValueError, tet_to_tri(volumes))
-
-    def test_tet_to_tri_corr_volumes(self):
-        volumes = np.array([[1, 2, 3, 3], [4, 5, 6, 5], [7, 8, 9, 5]])
-        expect_data = [[1, 3, 2],
-                       [2, 3, 1],
-                       [3, 3, 2],
-                       [3, 3, 1],
-                       [4, 6, 5],
-                       [5, 5, 4],
-                       [6, 5, 5],
-                       [5, 6, 4],
-                       [7, 9, 8],
-                       [8, 5, 7],
-                       [9, 5, 8],
-                       [5, 9, 7]]
-        self.assertTrue(np.equal(expect_data, tet_to_tri(volumes)).all())
+def test_tet_to_tri_throwException(sample_1d_array):
+    # TODO this is a bug!!
+    with pytest.raises(IndexError):
+        tet_to_tri(sample_1d_array)
 
 
-class TestHexaToQuadFunction(unittest.TestCase):
-
-    def test_hexa_to_quad_throwException(self):
-        volumes = np.array([0, 5, 4, 4, 6, 8, 8, 9])
-        hexa_to_quad(volumes)
-
-    def test_hexa_to_quad_expect_throwException_raiseValueError(self):
-        data_nd_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [7, 8, 9]])
-        self.assertRaises(ValueError, hexa_to_quad(data_nd_array))
-
-    def test_hexa_to_quad_corr_volume(self):
-        volumes = np.array([[1, 2, 3, 3, 3, 3, 4, 5],
-                         [1, 2, 3, 3, 3, 3, 4, 5],
-                         [1, 2, 3, 3, 3, 3, 4, 5],
-                         [1, 2, 3, 3, 3, 3, 4, 5]])
-
-        expect_data = [[2, 1, 3, 3],
-                       [1, 2, 3, 3],
-                       [2, 3, 4, 3],
-                       [3, 3, 5, 4],
-                       [3, 1, 3, 5],
-                       [3, 3, 4, 5],
-                       [2, 1, 3, 3],
-                       [1, 2, 3, 3],
-                       [2, 3, 4, 3],
-                       [3, 3, 5, 4],
-                       [3, 1, 3, 5],
-                       [3, 3, 4, 5],
-                       [2, 1, 3, 3],
-                       [1, 2, 3, 3],
-                       [2, 3, 4, 3],
-                       [3, 3, 5, 4],
-                       [3, 1, 3, 5],
-                       [3, 3, 4, 5],
-                       [2, 1, 3, 3],
-                       [1, 2, 3, 3],
-                       [2, 3, 4, 3],
-                       [3, 3, 5, 4],
-                       [3, 1, 3, 5],
-                       [3, 3, 4, 5]]
-
-        self.assertTrue(np.equal(expect_data, hexa_to_quad(volumes)).all())
+def test_tet_to_tri_expect_raiseValueError(sample_tri_error):
+    with pytest.raises(ValueError):
+        tet_to_tri(sample_tri_error)
 
 
-class TestFacesToEdgesFunction(unittest.TestCase):
-    def test_faces_to_edges_throwException(self):
-        pass
+def test_tet_to_tri_corr_volumes(sample_tet, expected_tet_result):
+    assert np.equal(expected_tet_result, tet_to_tri(sample_tet)).all()
 
 
-class TestMakeQuadFacesFunction(unittest.TestCase):
+def test_hexa_to_quad_throwException(sample_1d_array):
+    # TODO this is a bug!
+    with pytest.raises(IndexError):
+        hexa_to_quad(sample_1d_array)
 
-    def test_make_quad_faces_throwException(self):
-        pass
+
+def test_hexa_to_quad_expect_throwException_raiseValueError(sample_hex_error):
+    with pytest.raises(ValueError):
+        hexa_to_quad(sample_hex_error)
 
 
-class TestMakeHexaVolumesFunction(unittest.TestCase):
+def test_hexa_to_quad_corr_volume(sample_hex_array, expected_quad_result):
+    assert np.equal(expected_quad_result, hexa_to_quad(sample_hex_array)).all()
 
-    def test_make_hexa_volumns_throwException(self):
-        pass
+
+def test_faces_to_edges_throwException(sample_1d_array):
+    # TODO this is a bug!
+    with pytest.raises(IndexError):
+        faces_to_edges(sample_1d_array)
+
+
+def test_faces_to_edges_faces_tet(sample_faces_tri, expected_edges_tri):
+    assert np.equal(expected_edges_tri, faces_to_edges(sample_faces_tri)).all()
+
+
+def test_faces_to_edges_feces_tri(sample_faces_tet, expected_edges_tet):
+    assert np.equal(expected_edges_tet, faces_to_edges(sample_faces_tet)).all()
+
+
+def test_make_quad_faces_throwException(sample_hex):
+    # TODO this is a bug!
+    with pytest.raises(ValueError):
+        make_quad_faces(sample_hex)
+
+
+def test_make_quad_faces(sample_quad_faces, expected_quad_faces):
+    assert np.equal(expected_quad_faces, make_quad_faces(sample_quad_faces)).all()
+
+
+def test_make_quad_faces_wrong_input(sample_quad_faces_fail):
+    # TODO this is a bug
+    with pytest.raises(ValueError):
+        make_quad_faces(sample_quad_faces_fail)
+
+
+def test_make_hexa_volumes_throwException(sample_hex_fail):
+    # TODO this is a bug!
+    with pytest.raises(ValueError):
+        make_hexa_volumes(sample_hex_fail)
+
+
+def test_make_hexa_volumes_suc(sample_hex, expected_hexa_volumes):
+    assert np.equal(expected_hexa_volumes, make_hexa_volumes(sample_hex)).all()
