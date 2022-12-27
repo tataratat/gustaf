060341dd8897d1703c541c6ab2ebad7d314ab5fd
Jaewook Lee <jaewooklee042@gmail.com>
2022-12-27 14:46:14 +0100

    fix status check logic

--- gustaf/spline/ffd.py
+++ gustaf/spline/ffd.py
@@ -397,8 +397,8 @@ class FFD(GustafBase):
             Returns, if applicable, the vedo plotter. 'close=False' as argument
             to get the plotter.
         """
-        if not (self._spline is None or self._mesh is None):
-            raise ValueError("Please set a spline before using this function.")
+        if (self._spline is None and self._mesh is None):
+            raise ValueError("Please set a mesh before calling show()")
         backend = kwargs.pop("backend", None)
         return_showable = kwargs.pop("return_showable", False)
         return_discrete = kwargs.pop("return_discrete", False)
