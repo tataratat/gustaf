1258bfe5f7a9434feabc7008bab78100a78accef
deltaLuki <lukas.thurner@hotmail.com>
2023-01-26 13:21:20 +0100

    Change parameter for in NutTile2D

--- gustaf/spline/microstructure/tiles/nuttile2d.py
+++ gustaf/spline/microstructure/tiles/nuttile2d.py
@@ -21,40 +21,33 @@ class NutTile2D(TileBase):
         self,
         parameters=None,
         parameter_sensitivities=None,
-        contact_length=0.5,
+        contact_length=0.49,
         **kwargs
     ):
+
         if not isinstance(contact_length, float):
             raise ValueError("Invalid Type for radius")
 
-        # if not ((wall_thickness > 0.01) and (wall_thickness < 0.99)):
-        #     raise ValueError(
-        #         "The thickness of the wall must be in (0.01 and 0.99)"
-        #     )
         if not ((contact_length > 0) and (contact_length < 0.99)):
             raise ValueError("The length of a side must be in (0.01, 0.99)")
 
         if parameters is None:
             self._logd("Setting parameters to default values (0.5)")
-            parameters = tuple([np.ones(1) * 0.4])
+            parameters = tuple([np.ones(1) * 0.49])
 
-        v_zero= 0.
+        v_h_void = parameters[0][0]
+        if not ((v_h_void > 0.01) and (v_h_void < 0.5)):
+            raise ValueError(
+                "The thickness of the wall must be in (0.01 and 0.49)"
+            )
+
+        v_zero = 0.
+        v_one_half = 0.5
         v_one = 1.
         v_outer_c_h = contact_length * 0.5
-        v_h_void = parameters[0][0]
         v_inner_c_h = contact_length * parameters[0][0]
 
-
-
-        center = 0.5
-        param = 1 - parameters[0][0]
-        delta_out = center * contact_length
-        # alpha = math.asin(delta_out / center)
-        delta_in = (delta_out  * param)
-
         spline_list = []
-        v_one_half = 0.5
-        assert(v_h_void < 0.5)
 
         # set points:
         right = np.array(
@@ -68,66 +61,66 @@ class NutTile2D(TileBase):
 
         right_top = np.array(
             [
-                [center, delta_out],
-                [center * param, delta_in],
-                [delta_out, center],
-                [delta_in, center * param],
+                [v_h_void + v_one_half, v_inner_c_h + v_one_half],
+                [v_one, v_outer_c_h + v_one_half],
+                [v_inner_c_h + v_one_half, v_h_void+v_one_half],
+                [v_outer_c_h + v_one_half, v_one],
             ]
-        ) + np.array([v_one_half, v_one_half])
-
-        bottom = np.array(
-            [
-                [delta_in, -center * param],
-                [delta_out, -center],
-                [-delta_in, -center * param],
-                [-delta_out, -center],
-            ]
-        ) + np.array([v_one_half, v_one_half])
-
-        bottom_left = np.array(
-            [
-                [-delta_in, -center * param],
-                [-delta_out, -center],
-                [-center * param, -delta_in],
-                [-center, -delta_out],
-            ]
-        ) + np.array([v_one_half, v_one_half])
-
-        left = np.array(
-            [
-                [-center, delta_out],
-                [-center * param, delta_in],
-                [-center, -delta_out],
-                [-center * param, -delta_in],
-            ]
-        ) + np.array([v_one_half, v_one_half])
-
-        top_left = np.array(
-            [
-                [-center, delta_out],
-                [-center * param, delta_in],
-                [-delta_out, center],
-                [-delta_in, center * param],
-            ]
-        ) + np.array([v_one_half, v_one_half])
+        )
 
         top = np.array(
             [
-                [delta_out, center],
-                [delta_in, center * param],
-                [-delta_out, center],
-                [-delta_in, center * param],
+                [v_inner_c_h + v_one_half, v_h_void + v_one_half],
+                [v_outer_c_h + v_one_half, v_one],
+                [-v_inner_c_h + v_one_half, v_h_void + v_one_half],
+                [-v_outer_c_h + v_one_half, v_one]
             ]
-        ) + np.array([v_one_half, v_one_half])
+        )
+
+        bottom_left = np.array(
+            [
+                [-v_h_void + v_one_half, -v_inner_c_h + v_one_half],
+                [v_zero, -v_outer_c_h + v_one_half],
+                [-v_inner_c_h + v_one_half, -v_h_void + v_one_half],
+                [-v_outer_c_h + v_one_half, v_zero]
+            ]
+        )
+
+        left = np.array(
+            [
+                [-v_h_void + v_one_half, -v_inner_c_h + v_one_half],
+                [v_zero, -v_outer_c_h + v_one_half],
+                [-v_h_void + v_one_half, v_inner_c_h + v_one_half],
+                [v_zero, v_outer_c_h + v_one_half]
+            ]
+        )
+
+        top_left = np.array(
+            [
+                [-v_h_void + v_one_half, v_inner_c_h + v_one_half],
+                [v_zero, v_outer_c_h + v_one_half],
+                [-v_inner_c_h + v_one_half, v_h_void + v_one_half],
+                [-v_outer_c_h + v_one_half, v_one]
+            ]
+        )
+
+        bottom = np.array(
+            [
+                [v_inner_c_h + v_one_half, -v_h_void + v_one_half],
+                [v_outer_c_h + v_one_half, v_zero],
+                [-v_inner_c_h + v_one_half, -v_h_void + v_one_half],
+                [-v_outer_c_h + v_one_half, v_zero]
+            ]
+        )
 
         bottom_right = np.array(
             [
-                [delta_out, -center],
-                [delta_in, -center * param],
-                [center, -delta_out],
-                [center * param, -delta_in],
+                [v_inner_c_h + v_one_half, -v_h_void + v_one_half],
+                [v_outer_c_h + v_one_half, v_zero],
+                [v_h_void + v_one_half, -v_inner_c_h + v_one_half],
+                [v_one, -v_outer_c_h + v_one_half]
             ]
-        ) + np.array([v_one_half, v_one_half])
+        )
 
         spline_list.append(base.Bezier(degrees=[1, 1], control_points=right))
 
