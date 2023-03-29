1ab49cede9827163ae82382cbc8ca5dbe4aab09f
jzwar <zwar@ilsb.tuwien.ac.at>
2023-03-29 13:50:42 +0200

    New Microtiles that got lost after the rebase
    @deltaLuki's new microtiles got tangled in the rebase, this squashes all commits and introduces new microtiles to the
    microstructure pipeline

--- gustaf/spline/microstructure/tiles/tilebase.py
+++ gustaf/spline/microstructure/tiles/tilebase.py
@@ -61,3 +61,60 @@ class TileBase(base.GustafBase):
                 "Inherited Tile-types need to provide _dim, see documentation."
             )
         return self._dim
+
+    def check_params(self, params):
+        """Checks if the parameters have the correct format and shape
+
+        Parameters
+        ----------
+        params: tuple(np.ndarray)
+            the parameters for the tile
+
+        Returns
+        -------
+        True: Boolean
+        """
+        # check if tuple
+        if not (isinstance(params, tuple)):
+            raise TypeError("parameters must be a Tuple with array as entries")
+        # check if the tuple has the correct number of entries
+        if not (len(params) == self._parameter_space_dimension):
+            raise TypeError(
+                f"Size mismatch in param, expected "
+                f"{self._parameter_space_dimension} "
+                f"entries got {len(params)} entries"
+            )
+        # check if all entries in the tuple has the correct length
+        if not (all([len(self._evaluation_points) == e.size for e in params])):
+            raise TypeError(
+                f"Mismatch amount of parameter entries, expected "
+                f"{self._evaluation_points}"
+            )
+
+        return True
+
+    def check_param_derivatives(self, derivatives):
+        """Checks if all derivatives have the correct format and shape
+
+        Parameters
+        ----------
+        derivatives: list(tuple(np.ndarray)
+            all derivatives as list
+
+        Returns
+        -------
+        True: Boolean
+        """
+        if derivatives is None:
+            return False
+
+        if not (isinstance(derivatives, list)):
+            raise TypeError(
+                f"The parameter_sensitives passed have the wrong "
+                "format. The expected format is list(tuple(np.ndarray)),"
+                f" found type({type(derivatives) })"
+            )
+        for i in derivatives:
+            self.check_params(i)
+
+        return True
