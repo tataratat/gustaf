a4baabb6255e58d3738620007d2d498d65ad6061
clemens.fricke <clemens.david.fricke@tuwien.ac.at>
2023-04-13 13:11:56 +0200

    Reverted to using os.path instead of pathlib

--- docs/source/extra_docs.py
+++ docs/source/extra_docs.py
@@ -5,17 +5,23 @@ Feel free to extend!
 Contents:
 1. Create markdown table of show options.
 """
-import pathlib
+import os
 
 import gustaf as gus
 
 if __name__ == "__main__":
-    here = pathlib.Path(__file__).parent.resolve()
+    here = os.path.dirname(os.path.abspath(__file__))
+
+    # here = pathlib.Path(__file__).parent.resolve()
     # create md dir
-    (here / "../md").resolve().mkdir(parents=True, exist_ok=True)
+    md_path = os.path.abspath(os.path.join(here, "..", "md"))
+    # (here / "../md").resolve().mkdir(parents=True, exist_ok=True)
+    os.makedirs(md_path, exist_ok=True)
 
     # 1. Show options.
-    with open((here / "../md/show_options.md").resolve(), "w") as f:
+    with open(
+        os.path.abspath(os.path.join(here, "..", "md", "show_options.md")), "w"
+    ) as f:
         derived = gus.helpers.options.ShowOption.__subclasses__()
         for cls in derived:
             f.write(f"## {cls.__qualname__}\n\n")
