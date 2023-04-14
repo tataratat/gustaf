d306a16bfafdcc961d6e4598ae02c6e0fbe8238e
Jaewook Lee <jaewooklee042@gmail.com>
2023-04-14 22:59:51 +0200

    try to reconnect if ping fails

--- gustaf/utils/comm.py
+++ gustaf/utils/comm.py
@@ -1,15 +1,18 @@
 """
 Communication utilities.
 """
-has_websockets = False
 try:
     # this requires python 3.7+
+    from websockets.exceptions import ConnectionClosed
     from websockets.sync.client import connect
+
     has_websockets = True
+
 except ImportError as err:
     from gustaf.helpers.raise_if import ModuleImportRaiser
 
     connect = ModuleImportRaiser("websockets", err)
+    has_websockets = False
 
 from gustaf._base import GustafBase
 
@@ -30,7 +33,7 @@ class WebSocketClient(GustafBase):
         Parameters
         ----------
         uri: str
-        close_timeout: int  
+        close_timeout: int
         """
         # save config in case we need to re connect
         self.uri = uri
@@ -38,8 +41,32 @@ class WebSocketClient(GustafBase):
         self.websocket = connect(uri, close_timeout=close_timeout)
 
     def send_recv(self, message):
+        """
+        Send message and return received answer.
+
+        Parameters
+        ----------
+        message: str
+
+        Returns
+        -------
+        response: str
+        """
+        try:
+            self.websocket.ping()
+        except (ConnectionClosed, RuntimeError):
+            self._logd("connection error. trying to reconnect.")
+            # try to reconnect
+            self.websocket = connect(
+                self.uri, close_timeout=self.close_timeout
+            )
+
         self.websocket.send(message)
+
         return self.websocket.recv()
 
     def __del__(self):
+        """
+        Call close before deleting
+        """
         self.websocket.close()
