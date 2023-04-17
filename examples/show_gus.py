7c1860d28b7838dc932a155a2e3327ce2f20e518
clemens.fricke <clemens.david.fricke@tuwien.ac.at>
2023-04-17 15:46:09 +0200

    Add multiple options to show_gus example like pupil and white oval background.

--- examples/show_gus.py
+++ examples/show_gus.py
@@ -2,26 +2,28 @@
 import gustaf as gus
 
 WITH_PUPILS = False
+WITH_DARK_BACKGROUND = False
+DARK_WITH_WHITE_OVAL_BACKGROUND = False
 
 if __name__ == "__main__":
     l_eye = gus.spline.create.disk(0.15)
     l_eye.cps += [0.6, 0.2]
-    l_eye.show_options["c"] = "white"
+    l_eye.show_options["c"] = "black"
 
     r_eye = gus.spline.create.disk(0.15)
     r_eye.cps += [-0.6, 0.2]
-    r_eye.show_options["c"] = "white"
+    r_eye.show_options["c"] = "black"
 
     x_offset = 0.59
     y_offset = 0.16
 
     l_pupil = gus.spline.create.disk(0.05)
     l_pupil.cps += [x_offset, y_offset]
-    l_pupil.show_options["c"] = "black"
+    l_pupil.show_options["c"] = "white"
 
     r_pupil = gus.spline.create.disk(0.05)
     r_pupil.cps += [-x_offset, y_offset]
-    r_pupil.show_options["c"] = "black"
+    r_pupil.show_options["c"] = "white"
 
     upperlip = gus.Bezier(
         [5, 1],
@@ -79,20 +81,42 @@ if __name__ == "__main__":
         control_points=False,
         knots=False,
         lighting="off",
-        background="black",
-        close=False,
-    )
-
-    l_eye.show_options["c"] = "black"
-    r_eye.show_options["c"] = "black"
-    l_pupil.show_options["c"] = "white"
-    r_pupil.show_options["c"] = "white"
-
-    plt = gus.show(
-        item_to_show,
-        control_points=False,
-        knots=False,
-        lighting="off",
         background="white",
         close=False,
     )
+
+    if WITH_DARK_BACKGROUND:
+        background = []
+
+        if DARK_WITH_WHITE_OVAL_BACKGROUND:
+            height = 0.6
+            width = 0
+
+            x_shift = 0.3
+            y_shift = -0.02
+
+            background = [
+                gus.spline.create.disk(height),
+                gus.spline.create.disk(height),
+                gus.spline.create.box(width + 2 * x_shift, 2 * height),
+            ]
+
+            background[0].cps += [-x_shift, y_shift]
+            background[1].cps += [width + x_shift, y_shift]
+            background[2].cps += [-x_shift, -height + y_shift]
+            for elem in background:
+                elem.show_options["c"] = "white"
+        else:
+            l_eye.show_options["c"] = "white"
+            r_eye.show_options["c"] = "white"
+            l_pupil.show_options["c"] = "black"
+            r_pupil.show_options["c"] = "black"
+
+        plt = gus.show(
+            [*background, *item_to_show],
+            control_points=False,
+            knots=False,
+            lighting="off",
+            background="black",
+            close=False,
+        )
