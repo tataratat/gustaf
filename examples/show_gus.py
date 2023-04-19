7b5686d98c5ceb16cc19c60faa075d88392d1ea7
clemens.fricke <clemens.david.fricke@tuwien.ac.at>
2023-04-19 17:17:08 +0200

    Add alpha-based background.

--- examples/show_gus.py
+++ examples/show_gus.py
@@ -4,8 +4,14 @@ import gustaf as gus
 WITH_PUPILS = False
 WITH_DARK_BACKGROUND = False
 DARK_WITH_WHITE_OVAL_BACKGROUND = False
+WITH_ALPHA_BACKGROUND = False
 
 if __name__ == "__main__":
+    if WITH_ALPHA_BACKGROUND:
+        import vedo
+
+        vedo.settings.screenshot_transparent_background = 1
+
     l_eye = gus.spline.create.disk(0.15)
     l_eye.cps += [0.6, 0.2]
     l_eye.show_options["c"] = "black"
@@ -25,7 +31,7 @@ if __name__ == "__main__":
     r_pupil.cps += [-x_offset, y_offset]
     r_pupil.show_options["c"] = "white"
 
-    upperlip = gus.Bezier(
+    upper_lip = gus.Bezier(
         [5, 1],
         [
             [-0.8, -0.1],
@@ -42,12 +48,12 @@ if __name__ == "__main__":
             [0.75, -0.15],
         ],
     )
-    upperlip.show_options["c"] = "orange7"
+    upper_lip.show_options["c"] = "orange7"
 
-    innermouth = gus.Bezier(
+    inner_mouth = gus.Bezier(
         [5, 1],
         [
-            *upperlip.cps[6:],
+            *upper_lip.cps[6:],
             [-0.75, -0.15],
             [-0.6, -0.325],
             [-0.4, -0.525],
@@ -56,12 +62,12 @@ if __name__ == "__main__":
             [0.75, -0.15],
         ],
     )
-    innermouth.show_options["c"] = "orange5"
+    inner_mouth.show_options["c"] = "orange5"
 
-    lowerlip = gus.Bezier(
+    lower_lip = gus.Bezier(
         [5, 1],
         [
-            *innermouth.cps[6:],
+            *inner_mouth.cps[6:],
             [-0.8, -0.1],
             [-0.6, -0.4],
             [-0.4, -0.6],
@@ -70,9 +76,9 @@ if __name__ == "__main__":
             [0.8, -0.1],
         ],
     )
-    lowerlip.show_options["c"] = "orange7"
+    lower_lip.show_options["c"] = "orange7"
 
-    item_to_show = [l_eye, r_eye, upperlip, innermouth, lowerlip]
+    item_to_show = [l_eye, r_eye, upper_lip, inner_mouth, lower_lip]
     if WITH_PUPILS:
         item_to_show += [l_pupil, r_pupil]
 
@@ -106,6 +112,7 @@ if __name__ == "__main__":
             background[2].cps += [-x_shift, -height + y_shift]
             for elem in background:
                 elem.show_options["c"] = "white"
+                elem.show_options["alpha"] = 0.25
         else:
             l_eye.show_options["c"] = "white"
             r_eye.show_options["c"] = "white"
