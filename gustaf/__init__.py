fa6456dcc25a372e9dacaebf54388ce2b03736bb
Jaewook Lee <jaewooklee042@gmail.com>
2023-06-21 14:55:01 +0200

    remove splines

--- gustaf/__init__.py
+++ gustaf/__init__.py
@@ -16,30 +16,6 @@ from gustaf.faces import Faces
 from gustaf.vertices import Vertices
 from gustaf.volumes import Volumes
 
-has_spline = False
-try:
-    from gustaf import spline
-    from gustaf.spline.base import NURBS, Bezier, BSpline, RationalBezier
-    from gustaf.spline.ffd import FFD
-
-    has_spline = True
-except ImportError as err:
-    # overwrites the all modules which depend on the `splinepy` library
-    # with an object which will throw an error
-    # as soon as it is used the first time. This means that any non spline
-    # functionality works as before, but as soon as these are used a
-    # comprehensive exception will be raised which is understandable in
-    # contrast to the possible multitude of errors previously possible
-    from gustaf.helpers.raise_if import ModuleImportRaiser
-
-    spline = ModuleImportRaiser("splinepy", err)
-    BSpline = spline
-    NURBS = spline
-    Bezier = spline
-    RationalBezier = spline
-    FFD = spline
-
-
 __version__ = _version.version
 
 __all__ = [
@@ -58,11 +34,4 @@ __all__ = [
     "Edges",
     "Faces",
     "Volumes",
-    "spline",
-    "has_spline",
-    "BSpline",
-    "NURBS",
-    "Bezier",
-    "RationalBezier",
-    "FFD",
 ]
