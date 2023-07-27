680646b1005fe7f33314ca4f1d79653fdfd3c28c
Roxana-P <roxana.pohlmann@tuwien.ac.at>
2023-07-27 13:30:32 +0200

    Feature: Export gustaf mesh with meshio (#138)
    * Add export function in io.meshio
    
    * Add option to add subgroups in meshio export
    
    * Review suggestions gustaf/io/meshio.py
    
    Co-authored-by: Jaewook Lee <47114801+j042@users.noreply.github.com>
    
    * Add example for meshio export
    
    * Update gustaf/io/meshio.py kwargs
    
    Co-authored-by: Jacques Zwar <63095162+jzwar@users.noreply.github.com>
    
    * Update gustaf/io/meshio.py
    
    Co-authored-by: Jacques Zwar <63095162+jzwar@users.noreply.github.com>
    
    * Update gustaf/io/meshio.py kwargs
    
    Co-authored-by: Jacques Zwar <63095162+jzwar@users.noreply.github.com>
    
    * Update gustaf/io/meshio.py exception text
    
    Co-authored-by: Jacques Zwar <63095162+jzwar@users.noreply.github.com>
    
    * Update gustaf/io/meshio.py exception text
    
    Co-authored-by: Jacques Zwar <63095162+jzwar@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add codeblock and link
    
    * Codeblock syntax
    
    * Update gustaf/io/meshio.py
    
    Co-authored-by: Jacques Zwar <63095162+jzwar@users.noreply.github.com>
    
    * official python name for arbitrary arguments
    
    * Update examples/export_meshio.py
    
    Co-authored-by: clemens.fricke <clemens.fricke@googlemail.com>
    
    * Update examples/export_meshio.py
    
    Co-authored-by: clemens.fricke <clemens.fricke@googlemail.com>
    
    * change link display
    
    ---------
    
    Co-authored-by: Jaewook Lee <47114801+j042@users.noreply.github.com>
    Co-authored-by: Jacques Zwar <63095162+jzwar@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: clemens.fricke <clemens.fricke@googlemail.com>

---
+++ examples/export_meshio.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import numpy as np
+
+import gustaf as gus
+
+if __name__ == "__main__":
+    export_path = Path("export")
+
+    # define coordinates
+    v = np.array(
+        [
+            [0.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [1.0, 1.0, 0.0],
+            [0.0, 0.0, 1.0],
+            [1.0, 0.0, 1.0],
+            [0.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0],
+        ]
+    )
+    # define tet connectivity
+    tv = np.array(
+        [
+            [0, 2, 7, 3],
+            [0, 2, 6, 7],
+            [0, 6, 4, 7],
+            [5, 0, 4, 7],
+            [5, 0, 7, 1],
+            [7, 0, 3, 1],
+        ]
+    )
+
+    # init tet
+    tet = gus.Volumes(
+        vertices=v,
+        volumes=tv,
+    )
+
+    fac = gus.Faces(vertices=v, faces=tv[:4, :3])
+
+    tet.vertex_data["arange"] = np.arange(len(tet.vertices))
+
+    # Export with subgroup triangles
+    gus.io.meshio.export(
+        tet, export_path / "export_meshio_subgroups.vtu", submeshes=[fac]
+    )
+    # The mesh still has to conform the needed format, e.g. stl will discard
+    # tetrahedra
+    gus.io.meshio.export(
+        tet, export_path / "export_meshio_subgroups.stl", submeshes=[fac]
+    )
+
+    # Export only tetrahedra
+    gus.io.meshio.export(tet, export_path / "export_meshio.vtu")
+    gus.io.meshio.export(tet.to_faces(), export_path / "export_meshio.stl")
