f464faa5fcbe4b9382cd46322192f62e5b860963
clemens.fricke <clemens.david.fricke@tuwien.ac.at>
2023-09-07 14:03:41 +0200

    Run_all_examples script hardened, against calling from outside of parent folder.

--- examples/run_all_examples.py
+++ examples/run_all_examples.py
@@ -1,14 +1,15 @@
 """Call all *.py files in the working directory.
 """
 import glob
+import pathlib
 import subprocess
 import sys
 
 if __name__ == "__main__":
     files_not_completed = []
 
-    for file in glob.glob("*.py"):
-        if file == "run_all_examples.py":
+    for file in glob.glob(str(pathlib.Path(__file__).parent) + "/*.py"):
+        if file.split("/")[-1] == "run_all_examples.py":
             continue
         print(f"Calling {file}")
         proc_return = subprocess.run([sys.executable, file])
