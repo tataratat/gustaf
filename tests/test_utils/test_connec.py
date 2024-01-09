c7229295a6640d55c52771ca9807e8d0ed9a0a0f
Jaewook Lee <jaewooklee042@gmail.com>
2024-01-09 23:10:24 +0100

    test WIP

--- tests/test_utils/test_connec.py
+++ tests/test_utils/test_connec.py
@@ -1,29 +1,103 @@
+import numpy as np
+
 import gustaf as gus
+
+shuffle = np.random.default_rng().shuffle
 
 
 def one_polygon():
     """
-    edge connectivity with one polygon
+    edge connectivity of one polygon
     """
-    pass
+    op = np.array(
+        [
+            [0, 1],
+            [1, 2],
+            [2, 3],
+            [4, 5],
+            [5, 0],
+        ],
+        dtype=int,
+    )
+    # shuffle before return
+    shuffle(op)
+
+    return op
+
 
 def two_polygons():
     """
+    edge connectivity of two polygons
     """
-    pass
+    tp = np.array(
+        [
+            # first, ascending order
+            [0, 1],
+            [1, 2],
+            [2, 3],
+            [4, 5],
+            [5, 0],
+            # second, descending.
+            [7, 6],
+            [8, 7],
+            [9, 8],
+            [10, 9],
+            [6, 10],
+        ],
+        dtype=int,
+    )
+
+    shuffle(tp)
+
+    return tp
+
 
 def one_line():
     """
+    edge connectivity of one line
     """
-    pass
+    ol = np.array(
+        [
+            [0, 1],
+            [1, 2],
+            [2, 3],
+            [4, 5],
+        ],
+        dtype=int,
+    )
+
+    shuffle(ol)
+
+    return ol
+
 
 def two_lines():
     """
+    edge connectivity of two lines
     """
-    pass
+    tl = np.array(
+        [
+            # first, ascending order
+            [0, 1],
+            [1, 2],
+            [2, 3],
+            [4, 5],
+            # second, descending.
+            [7, 6],
+            [8, 7],
+            [9, 8],
+            [10, 9],
+        ],
+        dtype=int,
+    )
+
+    shuffle(tl)
+
+    return tl
 
 
 def test_edges_to_polygons():
     """
+    test 
     """
     pass
