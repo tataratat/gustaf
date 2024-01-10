9e4c112df7261a36eb41687ad98fb7220655cc41
Jaewook Lee <jaewooklee042@gmail.com>
2024-01-10 16:01:56 +0100

    add more connec tests

--- tests/test_utils/test_connec.py
+++ tests/test_utils/test_connec.py
@@ -3,6 +3,7 @@ import numpy as np
 import gustaf as gus
 
 shuffle = np.random.default_rng().shuffle
+randint = np.random.default_rng().integers
 
 
 def one_polygon():
@@ -165,3 +166,76 @@ def test_sequentialize_edges():
         assert not ip
     assert seq[0] == [0, 1, 2, 3, 4, 5]
     assert seq[1] == [6, 7, 8, 9, 10]
+
+
+def test_tet_to_tri():
+    query = randint(0, 1000, (100, 4))
+    expected = []
+    for q in query:
+        # this is connectivity stated in docstring
+        expected.append(q[[0, 2, 1, 1, 3, 0, 2, 3, 1, 3, 2, 0]])
+
+    expected = np.vstack(expected).reshape(-1, 3)
+
+    assert (gus.utils.connec.tet_to_tri(query) == expected).all()
+
+
+def test_hexa_to_quad():
+    query = randint(0, 1000, (100, 8))
+    expected = []
+    for q in query:
+        # this is connectivity stated in docstring
+        expected.append(
+            q[
+                [
+                    1,
+                    0,
+                    3,
+                    2,
+                    0,
+                    1,
+                    5,
+                    4,
+                    1,
+                    2,
+                    6,
+                    5,
+                    2,
+                    3,
+                    7,
+                    6,
+                    3,
+                    0,
+                    4,
+                    7,
+                    4,
+                    5,
+                    6,
+                    7,
+                ]
+            ]
+        )
+
+    expected = np.vstack(expected).reshape(-1, 4)
+
+    assert (gus.utils.connec.hexa_to_quad(query) == expected).all()
+
+
+def test_faces_to_edges():
+    # this actually should work for any number of nodes
+    for i in range(3, 10):
+        query = randint(0, 1000, (100, i))
+
+        expected = []
+        for q in query:
+            # this should connect nodes from 0 to last and close it
+            edges = [[q[0]]]
+            for qq in q[1:]:
+                edges[-1].append(qq)
+                edges.append([qq])
+            edges[-1].append(q[0])
+            expected.append(edges)
+
+    expected = np.vstack(expected).reshape(-1, 2)
+
+    assert (gus.utils.connec.faces_to_edges(query) == expected).all()
