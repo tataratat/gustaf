c9e5f7c4c62a4263081b47b5cb21c9b82893be9d
Michael Kofler <michael.kofler@tuwien.ac.at>
2024-08-23 12:20:01 +0200

    added tests for 2D MFEM export

--- tests/test_export.py
+++ tests/test_export.py
@@ -7,6 +7,8 @@ import pytest
 import gustaf as gus
 
 all_grids = (
+    ("faces_tri_2d", "mfem_triangles_2d.mesh"),
+    ("faces_quad_2d", "mfem_quadrilaterals_2d.mesh"),
     ("volumes_hexa", "mfem_hexahedra_3d.mesh"),
     ("volumes_tet", "mfem_tetrahedra_3d.mesh"),
 )
@@ -19,20 +21,33 @@ def test_mfem_export(to_tmpf, are_stripped_lines_same, grid, request):
 
     verts = mesh.vertices
 
-    faces = mesh.to_faces(False)
-    boundary_faces = faces.single_faces()
+    if mesh.whatami in ("tri", "quad"):
+        lines = mesh.to_edges(False)
+        BC = {1: [], 2: [], 3: []}
+        for i in lines.single_edges():
+            # mark boundaries at x = 0 with 1
+            if np.max(verts[lines.const_edges[i], 0]) < 0.1:
+                BC[1].append(i)
+            # mark boundaries at x = 1 with 2
+            elif np.min(verts[lines.const_edges[i], 0]) > 0.9:
+                BC[2].append(i)
+            # mark rest of the boundaries with 3
+            else:
+                BC[3].append(i)
+    elif mesh.whatami in ("hexa", "tet"):
+        faces = mesh.to_faces(False)
 
-    BC = {1: [], 2: [], 3: []}
-    for i in boundary_faces:
-        # mark boundaries at x = 0 with 1
-        if np.max(verts[faces.const_faces[i], 0]) < 0.1:
-            BC[1].append(i)
-        # mark boundaries at x = 1 with 2
-        elif np.min(verts[faces.const_faces[i], 0]) > 0.9:
-            BC[2].append(i)
-        # mark rest of the boundaries with 3
-        else:
-            BC[3].append(i)
+        BC = {1: [], 2: [], 3: []}
+        for i in faces.single_faces():
+            # mark boundaries at x = 0 with 1
+            if np.max(verts[faces.const_faces[i], 0]) < 0.1:
+                BC[1].append(i)
+            # mark boundaries at x = 1 with 2
+            elif np.min(verts[faces.const_faces[i], 0]) > 0.9:
+                BC[2].append(i)
+            # mark rest of the boundaries with 3
+            else:
+                BC[3].append(i)
 
     mesh.BC = BC
 
