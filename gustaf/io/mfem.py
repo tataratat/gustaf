e0722c67429d0289e974c0e65452003f56cab953
Michael Kofler <michael.kofler@tuwien.ac.at>
2024-08-23 12:35:50 +0200

    split internal mfem export functions to 2D and 3D

--- gustaf/io/mfem.py
+++ gustaf/io/mfem.py
@@ -107,6 +107,22 @@ def load(fname):
         return mesh
 
 
+def format_array(array):
+    """Format NumPy array into string. Each entry in a row is separated by
+    a blank space and every row is separated by a new line.
+
+    Parameters
+    ------------
+    array: NumPy array
+
+    Returns
+    ------------
+    str
+    """
+    row_strings = [" ".join(map(str, row)) for row in array]
+    return "\n".join(row_strings)
+
+
 def export(fname, mesh):
     """Export mesh in MFEM format. Supports 2D triangle and quadrilateral
     meshes as well as 3D tetrahedral meshes. Handles boundary conditions for
@@ -126,137 +142,144 @@ def export(fname, mesh):
     ------------
     None
     """
-    # Basic infos
-    nvertices, dim = mesh.vertices.shape
+    # Get dimension info based on vertices and choose corresponding export
+    dim = mesh.vertices.shape[1]
 
-    def format_array(array):
-        """Format NumPy array into string. Each entry in a row is separated by
-        a blank space and every row is separated by a new line.
-
-        Parameters
-        ------------
-        array: NumPy array
-
-        Returns
-        ------------
-        str
-        """
-        row_strings = [" ".join(map(str, row)) for row in array]
-        return "\n".join(row_strings)
-
-    # Export 2D mesh
     if dim == 2:
-        # Elements
-        element_attribute = 1  # Other numbers not yet supported
-        elements = mesh.elements
-        n_elements = elements.shape[0]
-        if mesh.whatami == "tri":
-            geometry_type = geometry_types["TRIANGLE"]
-        elif mesh.whatami == "quad":
-            geometry_type = geometry_types["SQUARE"]
-        else:
-            raise NotImplementedError(
-                f"Sorry, we cannot export 2D meshes of type {mesh.whatami}."
-            )
-        e = np.ones((n_elements, 1), dtype=settings.INT_DTYPE)
-        elements_array = np.hstack(
-            (element_attribute * e, geometry_type * e, elements)
-        )
-        elements_array_string = format_array(elements_array)
-        elements_string = f"elements\n{n_elements}\n"
-        elements_string += f"{elements_array_string}\n\n"
-
-        # Boundary
-        edges = mesh.edges()
-        nboundary_edges = sum(map(len, mesh.BC.values()))
-        boundary_array = np.empty(
-            (nboundary_edges, 4), dtype=settings.INT_DTYPE
-        )
-        startrow = 0
-        # Add boundary one by one as SEGMENTs
-        for bid, edgeids in mesh.BC.items():
-            nedges = len(edgeids)
-            e = np.ones(nedges).reshape(-1, 1)
-            vertex_list = edges[edgeids, :]
-            boundary_array[startrow : (startrow + nedges), :] = np.hstack(
-                (int(bid) * e, geometry_types["SEGMENT"] * e, vertex_list)
-            )
-            startrow += nedges
-
-        boundary_array_string = format_array(boundary_array)
-        boundary_string = f"boundary\n{nboundary_edges}\n"
-        boundary_string += f"{boundary_array_string}\n\n"
-
-        # Vertices
-        vdim = 2  # Currently only option
-        vertices_array_string = format_array(mesh.vertices)
-        vertices_string = f"vertices\n{nvertices}\n{vdim}\n"
-        vertices_string += f"{vertices_array_string}"
-
-        with open(fname, "w") as f:
-            f.write("MFEM mesh v1.0\n\n")
-            f.write(f"dimension\n{dim}\n\n")
-            f.write(elements_string)
-            f.write(boundary_string)
-            f.write(vertices_string)
-
-    # Export 3D mesh
+        elements_string, boundary_string, vertices_string = _export_2d(mesh)
+    elif dim == 3:
+        elements_string, boundary_string, vertices_string = _export_3d(mesh)
     else:
-        # Elements
-        element_attribute = 1  # Other numbers not yet supported
-        elements = mesh.elements
-        n_elements = elements.shape[0]
-        if mesh.whatami == "tet":
-            body_geometry_type = geometry_types["TETRAHEDRON"]
-            face_geometry_type = geometry_types["TRIANGLE"]
-            n_face_vertices = 3
-        elif mesh.whatami == "hexa":
-            body_geometry_type = geometry_types["CUBE"]
-            face_geometry_type = geometry_types["SQUARE"]
-            n_face_vertices = 4
-        else:
-            raise NotImplementedError(
-                f"Sorry, we cannot export 3D meshes of type {mesh.whatami}."
-            )
-        e = np.ones((n_elements, 1), dtype=settings.INT_DTYPE)
-        elements_array = np.hstack(
-            (element_attribute * e, body_geometry_type * e, elements)
+        raise NotImplementedError(
+            f"Sorry, we cannot export meshes of dimension {dim}."
         )
-        elements_array_string = format_array(elements_array)
-        elements_string = f"elements\n{n_elements}\n"
-        elements_string += f"{elements_array_string}\n\n"
 
-        # Boundary
-        faces = mesh.faces()
+    with open(fname, "w") as f:
+        f.write("MFEM mesh v1.0\n\n")
+        f.write(f"dimension\n{dim}\n\n")
+        f.write(elements_string)
+        f.write(boundary_string)
+        f.write(vertices_string)
 
-        nboundary_faces = sum(map(len, mesh.BC.values()))
-        boundary_array = np.empty(
-            (nboundary_faces, n_face_vertices + 2), dtype=settings.INT_DTYPE
+
+def _export_2d(mesh):
+    """Internal function to export 2D meshes.
+
+    Parameters
+    ------------
+    mesh: Faces
+
+    Returns
+    ------------
+    elements_string, boundary_string, vertices_string
+    """
+    element_attribute = 1  # Other numbers not yet supported
+    elements = mesh.elements
+    n_elements = elements.shape[0]
+    if mesh.whatami == "tri":
+        geometry_type = geometry_types["TRIANGLE"]
+    elif mesh.whatami == "quad":
+        geometry_type = geometry_types["SQUARE"]
+    else:
+        raise NotImplementedError(
+            f"Sorry, we cannot export 2D meshes of type {mesh.whatami}."
         )
-        startrow = 0
-        # Add boundary one by one as TRIANGLEs
-        for bid, faceids in mesh.BC.items():
-            nfaces = len(faceids)
-            e = np.ones(nfaces).reshape(-1, 1)
-            vertex_list = faces[faceids, :]
-            boundary_array[startrow : (startrow + nfaces), :] = np.hstack(
-                (int(bid) * e, face_geometry_type * e, vertex_list)
-            )
-            startrow += nfaces
+    e = np.ones((n_elements, 1), dtype=settings.INT_DTYPE)
+    elements_array = np.hstack(
+        (element_attribute * e, geometry_type * e, elements)
+    )
+    elements_array_string = format_array(elements_array)
+    elements_string = f"elements\n{n_elements}\n"
+    elements_string += f"{elements_array_string}\n\n"
 
-        boundary_array_string = format_array(boundary_array)
-        boundary_string = f"boundary\n{nboundary_faces}\n"
-        boundary_string += f"{boundary_array_string}\n\n"
+    # Boundary
+    edges = mesh.edges()
+    nboundary_edges = sum(map(len, mesh.BC.values()))
+    boundary_array = np.empty((nboundary_edges, 4), dtype=settings.INT_DTYPE)
+    startrow = 0
+    # Add boundary one by one as SEGMENTs
+    for bid, edgeids in mesh.BC.items():
+        nedges = len(edgeids)
+        e = np.ones(nedges).reshape(-1, 1)
+        vertex_list = edges[edgeids, :]
+        boundary_array[startrow : (startrow + nedges), :] = np.hstack(
+            (int(bid) * e, geometry_types["SEGMENT"] * e, vertex_list)
+        )
+        startrow += nedges
 
-        # Vertices
-        vdim = 3  # Currently only option
-        vertices_array_string = format_array(mesh.vertices)
-        vertices_string = f"vertices\n{nvertices}\n{vdim}\n"
-        vertices_string += f"{vertices_array_string}"
+    boundary_array_string = format_array(boundary_array)
+    boundary_string = f"boundary\n{nboundary_edges}\n"
+    boundary_string += f"{boundary_array_string}\n\n"
 
-        with open(fname, "w") as f:
-            f.write("MFEM mesh v1.0\n\n")
-            f.write(f"dimension\n{dim}\n\n")
-            f.write(elements_string)
-            f.write(boundary_string)
-            f.write(vertices_string)
+    # Vertices
+    vdim = 2  # Currently only option
+    vertices_array_string = format_array(mesh.vertices)
+    nvertices = mesh.vertices.shape[0]
+    vertices_string = f"vertices\n{nvertices}\n{vdim}\n"
+    vertices_string += f"{vertices_array_string}"
+    return elements_string, boundary_string, vertices_string
+
+
+def _export_3d(mesh):
+    """Internal function to export 3D meshes.
+
+    Parameters
+    ------------
+    mesh: Volumes
+
+    Returns
+    ------------
+    elements_string, boundary_string, vertices_string
+    """
+    element_attribute = 1  # Other numbers not yet supported
+    elements = mesh.elements
+    n_elements = elements.shape[0]
+    if mesh.whatami == "tet":
+        body_geometry_type = geometry_types["TETRAHEDRON"]
+        face_geometry_type = geometry_types["TRIANGLE"]
+        n_face_vertices = 3
+    elif mesh.whatami == "hexa":
+        body_geometry_type = geometry_types["CUBE"]
+        face_geometry_type = geometry_types["SQUARE"]
+        n_face_vertices = 4
+    else:
+        raise NotImplementedError(
+            f"Sorry, we cannot export 3D meshes of type {mesh.whatami}."
+        )
+    e = np.ones((n_elements, 1), dtype=settings.INT_DTYPE)
+    elements_array = np.hstack(
+        (element_attribute * e, body_geometry_type * e, elements)
+    )
+    elements_array_string = format_array(elements_array)
+    elements_string = f"elements\n{n_elements}\n"
+    elements_string += f"{elements_array_string}\n\n"
+
+    # Boundary
+    faces = mesh.faces()
+
+    nboundary_faces = sum(map(len, mesh.BC.values()))
+    boundary_array = np.empty(
+        (nboundary_faces, n_face_vertices + 2), dtype=settings.INT_DTYPE
+    )
+    startrow = 0
+    # Add boundary one by one as TRIANGLEs
+    for bid, faceids in mesh.BC.items():
+        nfaces = len(faceids)
+        e = np.ones(nfaces).reshape(-1, 1)
+        vertex_list = faces[faceids, :]
+        boundary_array[startrow : (startrow + nfaces), :] = np.hstack(
+            (int(bid) * e, face_geometry_type * e, vertex_list)
+        )
+        startrow += nfaces
+
+    boundary_array_string = format_array(boundary_array)
+    boundary_string = f"boundary\n{nboundary_faces}\n"
+    boundary_string += f"{boundary_array_string}\n\n"
+
+    # Vertices
+    vdim = 3  # Currently only option
+    vertices_array_string = format_array(mesh.vertices)
+    nvertices = mesh.vertices.shape[0]
+    vertices_string = f"vertices\n{nvertices}\n{vdim}\n"
+    vertices_string += f"{vertices_array_string}"
+    return elements_string, boundary_string, vertices_string
